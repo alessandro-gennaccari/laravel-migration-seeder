8dbd22021ba9053b8c91100fe4acf0b1b217760b
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-22 13:48:34 +0100

    added migration

--- database/migrations/2021_03_22_123915_create_students_table.php
+++ database/migrations/2021_03_22_123915_create_students_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateStudentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name',50);
+            $table->string('lastname',50);
+            $table->date('born');
+            $table->string('city',100);
+            $table->string('address');
+            $table->string('phone',40);
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('students');
     }
 }
